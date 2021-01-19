8bf404c6bb47e20da2a0f11e7c386ca59dbb3af9
goutham-o <studygouthamm@gmail.com>
2021-01-19 06:33:35 +0530

    added view pages and routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
         formsCSS: true,
@@ -16,16 +16,13 @@ exports.postAddProduct = (req, res, next) => {
     res.redirect('/');
 }
 
+
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
-        res.render('shop', {
+        res.render('admin/products', {
             prods: products,
-            path: '/',
-            pageTitle: 'shop',
-            hasProducts: products.length > 0,
-            activeShop: true,
-            productCSS: true
-        });
-    });
-
+            pageTitle: 'Products',
+            path: '/admin/products'
+        })
+    })
 }
