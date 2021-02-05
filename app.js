b40b4fc985c47c17905c78276e06ddbb39b69fb5
goutham-o <studygouthamm@gmail.com>
2021-02-06 00:13:41 +0530

    sequelize database

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const erorrController = require('./controllers/error');
-const db = require('./util/database');
+
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -14,13 +15,6 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-/* db.execute('SELECT * FROM products')
-    .then((result) => {
-        console.log(result[0]);
-    })
-    .catch(err => {
-        console.log(err);
-    }); */
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -30,5 +24,12 @@ app.use(shopRoutes);
 
 app.use(erorrController.get404);
 
-app.listen(3000);
+sequelize.sync()
+    .then(result => {
+
+        app.listen(3000);
+    })
+    .catch(err => console.log(err));
+
+
 
